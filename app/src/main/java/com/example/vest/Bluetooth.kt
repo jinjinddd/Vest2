2927b4961930aba9cb7d173bfccc147a143674f1
jinjinddd <ksj2512632@naver.com>
2022-04-26 17:14:01 +0900

    first

---
+++ app/src/main/java/com/example/vest/Bluetooth.kt
@@ -0,0 +1,18 @@
+package com.example.vest
+
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ToggleButton
+import androidx.core.app.ActivityCompat
+
+class Bluetooth : AppCompatActivity() {
+
+
+}
